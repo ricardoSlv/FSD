6bf7317d7017120754d92e75ed84e3d69a0105d9
Ricardo Silva <ricardoslv80@gmail.com>
2020-11-16 19:06:38 +0000

    G5 5.1 done

---
+++ g6/atomix/readme.md
@@ -0,0 +1,3 @@
+# Run Program
+
+mvn clean compile exec:java -Dexec.mainClass=ChatPeer
---
+++ g6/atomix/src/main/java/ChatPeer.java
@@ -0,0 +1,56 @@
+//import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+//import java.util.ArrayList;
+import io.atomix.cluster.messaging.MessagingConfig;
+import io.atomix.cluster.messaging.impl.NettyMessagingService;
+import io.atomix.utils.net.Address;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+//import java.util.concurrent.TimeUnit;
+
+public class ChatPeer {
+
+
+    public static void main(String[] args)  {
+        
+        String port = System.console().readLine();
+        String peer1 = System.console().readLine();
+        String peer2 = System.console().readLine();
+        String[] peers = {port,peer1,peer2};
+
+        ScheduledExecutorService es = Executors.newScheduledThreadPool(1);
+        MessagingConfig mConfig = new MessagingConfig();
+        mConfig.setPort(Integer.parseInt(port));
+        NettyMessagingService ms = new NettyMessagingService("chatpeer", Address.from(port),mConfig); 
+        
+        try {
+            Thread.sleep(100, 0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ms.registerHandler("chatpeer", (adress, msg) -> {
+            String decodedMsg = new String(msg, StandardCharsets.UTF_8);
+            System.out.println(decodedMsg);
+        }, es);
+
+        ms.start();
+
+        while (true) {
+            String msg = System.console().readLine();
+            for(String peer : peers){
+                ms.sendAsync(
+                    Address.from("localhost", Integer.parseInt(peer)), 
+                    "chatpeer", 
+                    msg.getBytes()
+                )
+                //.thenRun(() -> {System.out.println("Mensagem enviada!");})
+                .exceptionally(t -> {
+                    t.printStackTrace();
+                    return null;
+                });
+            }
+        }
+    }
+}
